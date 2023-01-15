b1184a32fbed06dbc0a3fc350790b44793a31909
James <entreur@gmail.com>
2023-01-15 18:08:13 +0400

    UI - Building.

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: Center(child: Text("Hello World")),
+        appBar: AppBar(
+          title: const Text("Project - Tracer"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Center(
+                  child: Text('Insert User Profile Here.',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              listItem(context, "Item 1", (() => print("Logger - Item 1"))),
+              listItem(context, "Item 2", (() => print("Logger - Item 2"))),
+              listItem(context, "Item 3", (() => print("Logger - Item 3"))),
+            ],
+          ),
+        ),
+        body: const Center(child: Text("Hello World")),
       ),
+    );
+  }
+
+  ListTile listItem(
+      BuildContext context, String itemname, VoidCallback method) {
+    return ListTile(
+      title: Text(itemname),
+      onTap: method,
+      // onTap: () {
+      //   print("Logger : - $itemname was pressed"); //REMOVE ME
+      //   Navigator.pop(context);
+      // },
     );
   }
 }
