c889c1b44c86ee5ddb3e510a92104604431505fe
James <entreur@gmail.com>
2023-01-15 20:14:11 +0400

    Code cleanup.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'UI/ui_home.dart';
 
+//Ensure no code is added here.
 void main() {
   runApp(const MyApp());
 }
-
-//Ensure no code is added here.
-
-
-
