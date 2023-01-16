fd04e1d5df82f005a31398e29d6e1cc2c347a82e
James <entreur@gmail.com>
2023-01-16 15:39:01 +0400

    Debug messages with time stamps.

---
+++ lib/Debug/d_log.dart
@@ -0,0 +1,6 @@
+// All debug functions should be located here.
+var date = DateTime.now();
+void debugOut(String itemname) {
+  // ignore: avoid_print
+  print("Logger: - Date: - $date $itemname");
+}
