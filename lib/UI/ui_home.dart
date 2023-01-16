8b9269ff79208ce70aa15439f3abde042080a3ce
James <entreur@gmail.com>
2023-01-16 17:15:48 +0400

    Moved drawer to its own class.

--- lib/UI/ui_home.dart
+++ lib/UI/ui_home.dart
@@ -1,6 +1,9 @@
 import 'package:bugtracker/Debug/d_log.dart';
-import 'package:bugtracker/UI/ui_item.dart';
+import 'package:bugtracker/UI/ui_appbar.dart';
+import 'package:bugtracker/UI/ui_drawer.dart';
 import 'package:flutter/material.dart';
+
+int currentPageIndex = 0;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,50 +14,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text("Project - Tracer"),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              DrawerHeader(
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: InkWell(
-                  // onTap: () => testMethod("Profile Clicked"),
-                  onTap: (() => debugOut("Profile Clicked")),
-                  child: Padding(
-                    padding: const EdgeInsets.all(0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: const [
-                        Flexible(
-                            child: CircleAvatar(
-                          maxRadius: 50,
-                          backgroundImage: NetworkImage(
-                              "https://randomuser.me/api/portraits/women/31.jpg"),
-                        )),
-                        // child: Image.network(
-                        //     "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg")),
-                        Flexible(
-                          child: Text("User 001"),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              ListItem("Dashboard", (() => debugOut("test 1"))),
-              ListItem("Profile", (() => debugOut("test 2"))),
-              ListItem("Logout", (() => debugOut("test 3"))),
-            ],
-          ),
+        appBar: const PersistentAppbar(),
+        drawer: const PersistentDrawer(),
+        //On Press a new window should be opened to allow user to insert ticket details
+        floatingActionButton: FloatingActionButton(
+          tooltip: "New Ticket",
+          child: const Icon(Icons.add),
+          onPressed: () => debugOut("Floating Action Button"),
         ),
         body: const Center(
           child: Text("Hello World", style: TextStyle(fontSize: 24)),
-        ), // body: const Center(child: Text("Hello World")),
+        ),
       ),
     );
   }
