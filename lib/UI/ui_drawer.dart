40be8f080c6a25c6ba17fc281cb6aa7207ba22b8
James <entreur@gmail.com>
2023-01-16 17:15:27 +0400

    Persistent drawer across pages.

---
+++ lib/UI/ui_drawer.dart
@@ -0,0 +1,47 @@
+import 'package:bugtracker/UI/ui_home.dart';
+import 'package:bugtracker/UI/ui_testpage.dart';
+import 'package:flutter/material.dart';
+import 'package:bugtracker/UI/ui_listitem.dart';
+import 'package:bugtracker/Debug/d_log.dart';
+
+class PersistentDrawer extends StatelessWidget {
+  const PersistentDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: InkWell(
+              onTap: (() => debugOut("Profile Clicked")),
+              child: Padding(
+                padding: const EdgeInsets.all(0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Flexible(
+                        child: CircleAvatar(
+                      maxRadius: 50,
+                      backgroundImage: NetworkImage(
+                          "https://randomuser.me/api/portraits/women/31.jpg"),
+                    )),
+                    Flexible(
+                      child: Text("User 001"),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ListItem("Home", const MyApp()),
+          ListItem("Test Page", const TestPage()),
+        ],
+      ),
+    );
+  }
+}
