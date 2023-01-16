5157ab30dd63837474aafbf65750abdb98e29329
James <entreur@gmail.com>
2023-01-16 19:44:11 +0400

    MyApp Class moved to its own class.

---
+++ lib/UI/ui_app.dart
@@ -0,0 +1,33 @@
+import 'package:bugtracker/Debug/d_log.dart';
+import 'package:bugtracker/UI/ui_appbar.dart';
+import 'package:bugtracker/UI/ui_drawer.dart';
+import 'package:bugtracker/UI/ui_home.dart';
+import 'package:flutter/material.dart';
+
+int currentPageIndex = 0;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: const PersistentAppbar(),
+        drawer: const PersistentDrawer(),
+        //On Press a new window should be opened to allow user to insert ticket details
+        floatingActionButton: FloatingActionButton(
+          tooltip: "New Ticket",
+          child: const Icon(Icons.add),
+          onPressed: () => debugOut("Floating Action Button"),
+        ),
+        body: HomePage(),
+        // body: const Center(
+        //   child: Text("Hello World", style: TextStyle(fontSize: 24)),
+        // ),
+      ),
+    );
+  }
+}
