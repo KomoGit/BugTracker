981fda04f8eab397a6b0063825668d2c237a295e
James <entreur@gmail.com>
2023-01-16 17:16:08 +0400

    Renamed item to ListItem

---
+++ lib/UI/ui_listitem.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class ListItem extends StatelessWidget {
+  final StatelessWidget page;
+  String itemname = "WARNING : Value is not given.";
+  ListItem(this.itemname, this.page, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(itemname),
+      onTap: () {
+        Navigator.pop(context);
+        Navigator.push(
+            context, MaterialPageRoute(builder: ((context) => page)));
+      },
+    );
+  }
+}
