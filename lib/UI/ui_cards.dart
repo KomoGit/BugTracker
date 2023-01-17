6282d510a66274f720167d3276e2b4d28608350d
James <entreur@gmail.com>
2023-01-17 14:45:27 +0400

    Cards that will display some info on dashboard.

---
+++ lib/UI/ui_cards.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DashboardCards extends StatelessWidget {
+  final int cardSize;
+  final Widget? childWidget;
+
+  const DashboardCards(this.cardSize, this.childWidget, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: (BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: Colors.white,
+          border: Border.all(width: 50, color: Colors.white),
+          borderRadius: const BorderRadius.all(Radius.circular(5)))),
+      child: childWidget,
+    );
+  }
+}
