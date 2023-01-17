f8aece54f5c593b4e7fb48e04233f1a7eaeb519b
James <entreur@gmail.com>
2023-01-17 14:45:42 +0400

    Cards that will display some info on dashboard.

--- lib/UI/ui_home.dart
+++ lib/UI/ui_home.dart
@@ -1,4 +1,6 @@
+import 'package:bugtracker/UI/ui_cards.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,9 +8,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      Row(
-        children: const [],
-      )
+      const Padding(padding: EdgeInsets.all(15)),
+      Row(children: [
+        const Padding(padding: EdgeInsets.all(15)),
+        Text(
+          "Dashboard",
+          style: GoogleFonts.montserrat(
+              color: Colors.white, fontSize: 24), //change the text color.
+        ),
+      ]),
+      const Padding(padding: EdgeInsets.all(10)),
+      Row(children: const [
+        Padding(
+          padding: EdgeInsets.all(15),
+        ),
+        DashboardCards(
+            40,
+            Center(
+              child: Text("LeBron Jaymes"),
+            )),
+        // Container(
+        //   decoration: (BoxDecoration(
+        //       shape: BoxShape.rectangle,
+        //       color: Colors.white,
+        //       border: Border.all(width: 50, color: Colors.white),
+        //       borderRadius: const BorderRadius.all(Radius.circular(5)))),
+        //   child: const Center(
+        //     child: Text("LeBron James"),
+        //   ),
+        // ),
+      ]),
     ]);
   }
 }
