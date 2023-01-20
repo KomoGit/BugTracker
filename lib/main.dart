ee6eae93ad5790209629a47d78f39fcb26487a72
James <entreur@gmail.com>
2023-01-20 16:16:52 +0400

    Access point for languages. Temporary.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:bugtracker/Localization/lang_select.dart';
 import 'package:flutter/material.dart';
 import 'UI/ui_app.dart';
 
 //Ensure no code is added here.
 void main() {
+  LangSelect lang = LangSelect();
+  lang.selectLanguage("ru");
   runApp(const MyApp());
 }
