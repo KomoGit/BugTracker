17e85b46357a35d0746e6c4201275b88d2e6a2c9
James <entreur@gmail.com>
2023-01-21 14:39:23 +0400

    Settings screen.

--- lib/UI/Screens/ui_settings.dart
+++ lib/UI/Screens/ui_settings.dart
@@ -1,10 +1,36 @@
+import 'package:bugtracker/UI/ui_appbar.dart';
+import 'package:bugtracker/UI/ui_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: const PersistentAppbar(),
+      drawer: const PersistentDrawer(),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            const Padding(padding: EdgeInsets.all(15)),
+            Row(
+              children: [
+                const Padding(padding: EdgeInsets.all(15)),
+                Center(
+                  child: Text(
+                    lang.getSettingsText!,
+                    style: GoogleFonts.montserrat(
+                        color: Colors.black, fontSize: 24),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
