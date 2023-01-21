3f704ef9270520242c2f31439a1ed5e0ce36073b
James <entreur@gmail.com>
2023-01-21 21:09:05 +0400

    Clean up.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:bugtracker/Localization/lang_select.dart';
 import 'package:bugtracker/UI/Screens/ui_app.dart';
 import 'package:flutter/material.dart';
 
 //Ensure no code is added here.
 void main() {
-  LangSelect lang = LangSelect();
-  lang.selectLanguage("ru");
+  lang.selectLanguage("en");
   runApp(const MyApp());
 }
