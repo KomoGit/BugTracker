0cbe82be0dc845e50fd3ade0773d460446bb5c90
James <entreur@gmail.com>
2023-01-23 19:38:47 +0400

    Minor change.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 //Ensure no code is added here.
 void main() {
-  lang.selectLanguage("en");
+  lang.selectLanguage(
+      "NULL"); //Fix this issue. Every time you restart the app goes to English (Default)
   runApp(const MyApp());
 }
