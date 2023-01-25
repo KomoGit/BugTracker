9b56095d3eecbd5ff63b6ce814354ae8af91466e
James <entreur@gmail.com>
2023-01-25 19:27:19 +0400

    Cleanup. Only runApp should exist here.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,5 @@ import 'package:flutter/material.dart';
 
 //Ensure no code is added here.
 void main() {
-  lang.selectLanguage(
-      "NULL"); //Fix this issue. Every time you restart the app goes to English (Default)
   runApp(const MyApp());
 }
