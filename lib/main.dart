c20cdff455a7b77a122e4084e5e2bfadd17c5666
James <entreur@gmail.com>
2023-01-25 20:23:27 +0400

    Reverting LangSelect. Causes bug.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:bugtracker/Localization/lang_select.dart';
 import 'package:bugtracker/UI/Screens/ui_app.dart';
 import 'package:flutter/material.dart';
 
 //Ensure no code is added here.
+LangSelect lang = LangSelect();
 void main() {
+  lang.selectLanguage("");
   runApp(const MyApp());
 }
