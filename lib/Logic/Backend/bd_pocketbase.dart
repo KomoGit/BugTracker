51cf8862b6e015734a8a2c313cb593cc076564a5
James <entreur@gmail.com>
2023-02-09 12:11:57 +0400

    Pocketbase get and set users.

--- lib/Logic/Backend/bd_pocketbase.dart
+++ lib/Logic/Backend/bd_pocketbase.dart
@@ -1,15 +1,22 @@
 import 'package:pocketbase/pocketbase.dart';
 import 'package:bugtracker/Model/model_user.dart';
 
-class MenuAPI {
+class GetSetUsers {
   //DO NOT MODIFY
   Future<List<User>> fromRecordsToModels(PocketBase pb) async {
-    var rawData = await pb.collection("menuitem").getFullList();
+    var rawData = await pb.collection('users').getFullList();
     List<RecordModel> listOfUser = rawData;
     List<User> categories = [];
     for (RecordModel model in listOfUser) {
-      categories.add(User.fromModel(model));
+      categories.add(User.recordToModel(model));
     }
     return categories;
+  }
+
+  //This might seem confusing but here is the jist. We take a model that contains data
+  //and from data we create a model.
+  Future<void> fromModelToRecord(
+      PocketBase pb, Map<String, dynamic> modelToRecord) async {
+    await pb.collection('users').create(body: modelToRecord);
   }
 }
