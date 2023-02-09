fc795d56c150ca4196c38a25755fdcd806d886bf
James <entreur@gmail.com>
2023-02-09 12:08:12 +0400

    Model for user.

--- lib/Model/model_user.dart
+++ lib/Model/model_user.dart
@@ -1,33 +1,32 @@
-// class Category {
-//   String id;
-//   String title;
-//   String imgUrl;
-
-//   Category(this.id, this.title, this.imgUrl);
-//   //Record Model is not native to Flutter, it is a part of PocketBase dependency.
-//   static Category fromModel(RecordModel model) {
-//     String id = model.id;
-//     String title = model.getStringValue('title');
-//     String imgUrl = model.getStringValue('imgUrl');
-//     return Category(id, title, imgUrl);
-//   }
-// }
-
 import 'package:pocketbase/pocketbase.dart';
 
 class User {
   String id;
   String fullName;
-  String imgUrl;
+  String profilePic;
   String email;
 
-  User(this.id, this.fullName, this.imgUrl, this.email);
-
-  static User fromModel(RecordModel model) {
+  User(this.id, this.fullName, this.profilePic, this.email);
+  //For Login.
+  static User recordToModel(RecordModel model) {
     String id = model.id;
     String fullName = model.getStringValue('fullname');
-    String imgUrl = model.getStringValue('profilepicurl');
+    String profilePic = model.getStringValue('profilePicture');
     String email = model.getStringValue('email');
-    return User(id, fullName, imgUrl, email);
+    return User(id, fullName, profilePic, email);
+  }
+
+  //For register.
+  static Map<String, dynamic> modelToRecord(String email, username, password) {
+    final body = <String, dynamic>{
+      "username": username,
+      "email": email,
+      "emailVisibility": true,
+      "password": password, //Will add a password confirmation check
+      "name": username,
+      "profilePicture":
+          "" //We will change this so users can apply their own urls
+    };
+    return body;
   }
 }
