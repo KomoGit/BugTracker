b657c9f4164664cbfe37888263591300c45660c8
James <entreur@gmail.com>
2023-02-16 13:34:02 +0400

    Code cleanup.

--- lib/Localization/lang_select.dart
+++ lib/Localization/lang_select.dart
@@ -2,7 +2,6 @@
 import 'package:bugtracker/Localization/Translation/tr_english.dart';
 import 'package:bugtracker/Localization/Translation/tr_russian.dart';
 import 'package:bugtracker/Localization/Translation/tr_turkish.dart';
-import 'package:bugtracker/Localization/lang_current.dart';
 
 class LangSelect {
   void selectLanguage(String selectedLang) {
@@ -29,54 +28,5 @@ class LangSelect {
 
   String? get getTextTurkish {
     return Turkish.lang;
-  }
-
-  /* 
-  Return text elements from here. 
-  This is access point for all texts.
-   */
-
-  String? get getYesText {
-    return CurrentStrings.currentYesText!;
-  }
-
-  String? get getNoText {
-    return CurrentStrings.currentNoText!;
-  }
-
-  String? get getSettingsText {
-    return CurrentStrings.currentSettingsText!;
-  }
-
-  String? get getFloatingActionText {
-    return CurrentStrings.currentFloatingActionToolTipText!;
-  }
-
-  String? get getDashboardText {
-    return CurrentStrings.currentDashboardText!;
-  }
-
-  String? get getDummyText {
-    return CurrentStrings.currentDummyText!;
-  }
-
-  String? get getAppBarText {
-    return CurrentStrings.currentAppBarText!;
-  }
-
-  String? get getHomeText {
-    return CurrentStrings.currentHomeText!;
-  }
-
-  String? get getTestPageText {
-    return CurrentStrings.currentTestPageText!;
-  }
-
-  String? get getUserGreetText {
-    return CurrentStrings.currentUserGreetText!;
-  }
-
-  String? get getChangeLanguageText {
-    return CurrentStrings.currentChangeLanguageText!;
   }
 }
